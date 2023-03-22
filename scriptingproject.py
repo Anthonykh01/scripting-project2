16e606c2034511e2b44da01df2968b66d024a819
anthonyykh <anthonykh01.ak@gmail.com>
2023-03-22 13:47:34 +0200

    this new code can take  a url from the user split it in order to add the subdomain name in the midlle thennputs it back together to check if we can get a response to the new url

--- scriptingproject.py
+++ scriptingproject.py
@@ -18,5 +18,19 @@
 # response = requests.get(url)
 
 # print(response.text)
-name = input('What is your name? ')
-print('Hello,', name)
+url = input('What is your url? ')
+print('the url is ,', url)
+with open('dirs_dictionary.bat', 'r') as file:
+    for line in file:
+        after_www = url.split("www.")[1]
+        new_url='www.'+line.strip()+'.'+after_www
+        print(new_url)
+
+        # url = 'https://www.lau.edu.lb'
+        # response = requests.get(url)
+
+        # if response.status_code == 200:
+        #     print('Request was successful.')
+        # else:
+        #     print('Request failed with status code:', response.status_code)
+        
