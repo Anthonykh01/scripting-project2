eca5808c9294c9859f000c7e7d8e5adc805bd608
anthonyykh <anthonykh01.ak@gmail.com>
2023-03-22 13:31:15 +0200

    tried the import function that i will use to ask thee user for the url he want to work on

--- scriptingproject.py
+++ scriptingproject.py
@@ -5,16 +5,18 @@
 
 # import requests
 
-import requests
+# import requests
 
-url = 'https://www.lau.edu.lb'
-response = requests.get(url)
+# url = 'https://www.lau.edu.lb'
+# response = requests.get(url)
 
-if response.status_code == 200:
-    print('Request was successful.')
-else:
-    print('Request failed with status code:', response.status_code)
+# if response.status_code == 200:
+#     print('Request was successful.')
+# else:
+#     print('Request failed with status code:', response.status_code)
 
 # response = requests.get(url)
 
 # print(response.text)
+name = input('What is your name? ')
+print('Hello,', name)
