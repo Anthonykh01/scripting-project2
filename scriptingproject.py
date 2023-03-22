43ddb639cbd016ff7a87c962179e80cfd1b1c8ed
anthonyykh <anthonykh01.ak@gmail.com>
2023-03-22 13:28:50 +0200

    tried the function that i will be usinf to get response back it is working  can add it to the for loop

---
+++ scriptingproject.py
@@ -0,0 +1,20 @@
+# with open('dirs_dictionary.bat', 'r') as file:
+#     for line in file:
+#         print(line)
+
+
+# import requests
+
+import requests
+
+url = 'https://www.lau.edu.lb'
+response = requests.get(url)
+
+if response.status_code == 200:
+    print('Request was successful.')
+else:
+    print('Request failed with status code:', response.status_code)
+
+# response = requests.get(url)
+
+# print(response.text)
