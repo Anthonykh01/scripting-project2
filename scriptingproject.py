be5953d9a273d1e3c04307a453d6d50d9f4cacaa
anthonyykh <anthonykh01.ak@gmail.com>
2023-03-22 18:53:44 +0200

    created a text file to store found directories and added the code to loop tghrough potential directories and store them in the text file if found

--- scriptingproject.py
+++ scriptingproject.py
@@ -20,41 +20,62 @@ import requests
 # print(response.text)
 with open("subdomain_success.txt", "w") as file:
     file.truncate(0)
+with open("directory_success.txt", "w") as file:
+    file.truncate(0)
+
 
 url = input('What is your url? ')
 print('the url is ,', url)
 after_www=url.split("www.")[1]
-with open('subdomains_dictionary.bat', 'r') as file:
+# with open('subdomains_dictionary.bat', 'r') as file:
+#     for line in file:
+         
+#         line = line.strip()
+        
+#         if line.endswith('.'):
+#             url = 'https://www.' + line[:-1] + '.' +after_www
+#         else:
+#             url = 'https://www.' + line + '.' + after_www
+#         try:
+#             response = requests.get(url)
+#             if response.status_code == 200:
+#                 print('Request was successful.')
+#                 print(url)
+#                 with open("subdomain_success.txt", "w") as file:
+#                     # Write the string to the file
+#                     file.write(url)
+#         except requests.exceptions.ConnectionError:
+#                 # handle the connection error
+#                 # print('no')
+#                 None
+#         #  else:
+#         #     print('Request failed with status code:', response.status_code)
+#         #     print(url)
+        
+with open('dirs_dictionary.bat', 'r') as file:
     for line in file:
          
         line = line.strip()
-        
-        if line.endswith('.'):
-            url = 'https://www.' + line[:-1] + '.' +after_www
-        else:
-            url = 'https://www.' + line + '.' + after_www
+        if line.startswith('.'):
+            line=line[1:]
+        url='https://' + url.strip() + '/'+ line
         try:
             response = requests.get(url)
             if response.status_code == 200:
                 print('Request was successful.')
                 print(url)
-                with open("output.txt", "w") as file:
+                with open("directory_success.txt", "w") as file:
                     # Write the string to the file
-                    file.write("This is a string that will be written to the file")
+                    file.write(url)
         except requests.exceptions.ConnectionError:
                 # handle the connection error
-                # print('no')
-                None
-        #  else:
-        #     print('Request failed with status code:', response.status_code)
-        #     print(url)
-        
-
+                 print('no')
+                # None
 # Open the file in write mode with truncate set to 0
-with open("output.txt", "w") as file:
-    file.truncate(0)
-    # Write the string to the file
-    file.write("This is a string that will be written to the file")
+# with open("output.txt", "w") as file:
+#     file.truncate(0)
+#     # Write the string to the file
+#     file.write("This is a string that will be written to the file")
 
 # try:
 #     response = requests.get(url)
